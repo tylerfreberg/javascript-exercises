b5734e56edb7057839909924c7509d5df8565b17
Tyler Freberg <33191810+tylerfreberg@users.noreply.github.com>
2026-03-02 14:44:35 -0600

    completed foundations 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    if (n < 0) {
+        return("ERROR");
+    }
 
+    let repeated = "";
+    for(i = 0; i < n; i++) {
+        repeated += str;
+    }
+    return(repeated);
 };
 
 // Do not edit below this line
