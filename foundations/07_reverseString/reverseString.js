20a35dc87fc5b2878f1c773d0ac3434a5ce6fc54
Tyler Freberg <33191810+tylerfreberg@users.noreply.github.com>
2026-03-02 14:49:12 -0600

    completed foundations 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    len = str.length;
+    let reversed = "";
+    for(i = len - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
 
+    return(reversed);
 };
 
 // Do not edit below this line
