93e96ebf288cdab69d8d8759cf71937a476cf00c
Tyler Freberg <tylerfreberg1@gmail.com>
2026-03-02 17:28:34 -0600

    added a solution for foundations 08. have yet to test it.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...intsForRemoval) {
+    if (intsForRemoval.length > arr.length) {
+        throw new Error("Amount of items for removal is longer than array length.");
+    }
+
+    filteredArr = []
+
+    for(i = 0; i < arr.length; i++) {
+        if (intsForRemoval.includes(arr[i]) == false) {
+            filteredArr.push(arr[i]);
+        }
+    }
+
+    return(filteredArr);
 };
 
 // Do not edit below this line
