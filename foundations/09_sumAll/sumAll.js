8f9b07b09482ef0c5d7e1d2bc70e664c2f058a47
Tyler Freberg <33191810+tylerfreberg@users.noreply.github.com>
2026-03-02 19:01:17 -0600

    completed foundations 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let start;
+    let end;
+    let sum = 0;
 
+    if (Number.isInteger(x) == false || Number.isInteger(y) == false ||
+        x < 0 || y < 0) {
+        return("ERROR");
+    }
+
+    if (x > y) {
+        start = y;
+        end = x;
+    }
+    else {
+        start = x;
+        end = y;
+    }
+
+    for(i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return(sum);
 };
 
 // Do not edit below this line
