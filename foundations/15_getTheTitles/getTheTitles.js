3ab38989cc76cdd7002bf80368eea24f1ec7a97a
Tyler Freberg <33191810+tylerfreberg@users.noreply.github.com>
2026-03-07 01:20:59 -0600

    completed foundations 15

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return(arr.map((book) =>returnTitle(book)));
 };
+
+function returnTitle(book) {
+    return book.title;
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
