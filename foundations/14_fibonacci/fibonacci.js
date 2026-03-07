1fc2f42d59338321c29546151fb38d34caf6a334
Tyler Freberg <33191810+tylerfreberg@users.noreply.github.com>
2026-03-07 01:15:11 -0600

    completed foundations 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let a = 0
+    let b = 1;
 
+    if (num == 0) {
+        return 0;
+    }
+    if (num < 0) {
+        return("OOPS");
+    }
+    for(i = 0; i < num; i++) {
+        let temp = a;
+        a = b;
+        b = temp + b;
+    }
+
+    return(a);
 };
 
 // Do not edit below this line
