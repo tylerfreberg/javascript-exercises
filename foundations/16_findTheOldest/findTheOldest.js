ba28e7aa0b24f25906b714ef4f6f882916eb699e
Tyler Freberg <33191810+tylerfreberg@users.noreply.github.com>
2026-03-07 01:37:40 -0600

    completed foundations 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+
+    let sorted = arr.sort((a, b) => calculateAge(a) < calculateAge(b) ? 1 : -1);
+    return sorted[0];
 };
+
+function calculateAge(obj) {
+    let deathYear;
+    if(obj.yearOfDeath === undefined) {
+        deathYear = new Date().getFullYear();
+    }
+    else {
+        deathYear = obj.yearOfDeath;
+    }
+
+    return(deathYear - obj.yearOfBirth);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
