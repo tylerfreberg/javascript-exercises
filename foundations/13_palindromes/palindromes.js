391db40eacd0a16f3afe4b94a330301e310185ea
Tyler Freberg <33191810+tylerfreberg@users.noreply.github.com>
2026-03-07 01:01:19 -0600

    completed foundations 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let replacedStr = (str.replace(/\W/g, '')).toLowerCase();
 
+    l = 0;
+    r = replacedStr.length - 1;
+
+    while(l < r) {
+        if(replacedStr[l] != replacedStr[r]) {
+            return false;
+        }
+        l += 1;
+        r -= 1;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
