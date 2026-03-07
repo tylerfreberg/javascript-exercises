f5cafd7d8a7e263f8f7719e6b0a4495ffccab0c7
Tyler Freberg <33191810+tylerfreberg@users.noreply.github.com>
2026-03-07 00:49:45 -0600

    completed foundations 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return(x+y);
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return(x-y)
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return(arr.reduce((total, val) => total + val, 0));
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return(arr.reduce((total, val) => total * val, 1));
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return(x**y);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+	for(i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+
+  return(factorial);
 };
 
 // Do not edit below this line
